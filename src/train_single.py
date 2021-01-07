8a86a0c1843b2028c2e98b8b0f7a52934375325f
nviolante <nviolante96@gmail.com>
2021-01-07 18:02:03 -0300

    Correcion salida feature extraction, separacion de clases

--- src/train_single.py
+++ src/train_single.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import pytorch_lightning as pl
+from torchvision.models.mobilenet import mobilenet_v2
 from torchvision.models.resnet import resnet18
 from torchvision.models.resnet import resnet101
 from utils.confusion_matrix import confusion_matrix_plot_as_array
@@ -18,7 +19,7 @@ class AgeResNet(pl.LightningModule):
         super().__init__()
         num_features = list(backbone.modules())[-1].in_features
         backbone = remove_last_layers(backbone, 1)
-        self.feature_extractor = nn.Sequential(backbone,nn.Flatten())
+        self.feature_extractor = nn.Sequential(backbone, nn.AdaptiveAvgPool2d((1,1)), nn.Flatten())
         self.fc_age = nn.Sequential(nn.Linear(num_features, 7), nn.LeakyReLU(0.2))
 
     def get_backbone(backbone, pretrained=True):
@@ -88,18 +89,30 @@ class AgeResNet(pl.LightningModule):
         return np.array(tensor.cpu(), dtype=np.uint8)
 
 
-class GenderResNet(AgeResNet):
+class GenderResNet(pl.LightningModule):
     def __init__(self, backbone):
         super().__init__()
         num_features = list(backbone.modules())[-1].in_features
         backbone = remove_last_layers(backbone, 1)
-        self.feature_extractor = nn.Sequential(backbone,nn.Flatten())
+        self.feature_extractor = nn.Sequential(backbone, nn.AdaptiveAvgPool2d((1,1)), nn.Flatten())
         self.fc_gender = nn.Sequential(nn.Linear(num_features, 2), nn.LeakyReLU(0.2))
 
     def forward(self, x):
         x = self.feature_extractor(x)
         x = F.softmax(self.fc_gender(x), dim=1)
         return x
+
+    def configure_optimizers(self):
+        return Adam(self.parameters(), lr=0.005)
+
+    def training_step(self, batch, batch_idx):
+        output = self.compute_output(batch, stage="train")
+        return output
+
+    def validation_step(self, batch, batch_idx):
+        output = self.compute_output(batch, stage="val")
+        return output
+
 
     def compute_output(self, batch, stage):
         image, _, gender = batch
@@ -127,9 +140,41 @@ class GenderResNet(AgeResNet):
         tb.add_scalar(f"{stage}/Gender accuracy", gender_accuracy, self.current_epoch)
         tb.add_image(f"{stage}/Gender", cm_gender, self.current_epoch, dataformats="HWC")
 
+    def training_epoch_end(self, outputs):
+        if self.is_time_to_log():
+            self.log_epoch_results(outputs, stage="train")
+
+    def validation_epoch_end(self, outputs):
+        if self.is_time_to_log():
+            self.log_epoch_results(outputs, stage="val")
+
+    def is_time_to_log(self, log_freq=10):
+        return self.current_epoch % log_freq == 0
+
+    def log_epoch_results(self, outputs, stage):
+        age_pred = []
+        age_true = []
+        for i, output in enumerate(outputs):
+            age_true.extend(self.tensor_to_array(output["age"][0]))
+            age_pred.extend(self.tensor_to_array(output["age"][1]))
+
+        age_labels = ["0-10", "11-20", "21-30", "31-40", "41-50", "51-60", "60-"]
+        cm_age = confusion_matrix_plot_as_array(age_true, age_pred, age_labels)
+
+        # Tensorboard logs
+        age_accuracy = accuracy(torch.tensor(age_pred).to(self.device), (torch.tensor(age_true).to(self.device))).item()
+
+        tb = self.logger.experiment
+        tb.add_scalar(f"{stage}/Age accuracy", age_accuracy, self.current_epoch)
+        tb.add_image(f"{stage}/Age", cm_age, self.current_epoch, dataformats="HWC")
+
+    @staticmethod
+    def tensor_to_array(tensor):
+        return np.array(tensor.cpu(), dtype=np.uint8)
+
 
 if __name__ == "__main__":
-    checkpoint = ModelCheckpoint(monitor='val/loss', save_top_k=1, save_last=True)
+    checkpoint = ModelCheckpoint(monitor='val/loss/gender', save_top_k=1, save_last=True)
     logger = TensorBoardLogger(save_dir=r"..\training", name="GenderResNet")
     trainer = pl.Trainer(gpus=1,
                          max_epochs=100,
@@ -138,7 +183,7 @@ if __name__ == "__main__":
 
 
     datamodule = DataModule("..\\data\\UTKFace", batch_size=4, num_workers=4)
-    backbone = resnet101(pretrained=True)
+    backbone = resnet18(pretrained=True)
     model = GenderResNet(backbone)
     trainer.fit(model, datamodule)
 
